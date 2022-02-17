a326705f5893f5636be01886608b020e85449966
Michael O'Donnell <Michael.ODonnell@fintru.com>
2022-02-17 17:05:09 +0000

    Creating interfaces for customer actions

---
+++ CustomerPortal/Controllers/Customers/ICustomerService.cs
@@ -0,0 +1,22 @@
+﻿using DataTransfer;
+using System.Threading.Tasks;
+
+namespace CustomerPortal.Controllers.Customers
+{
+    public interface ICustomerService
+    {
+        /// <summary>
+        /// Get a Customer
+        /// </summary>
+        /// <param name="customerId">ID of a Customer</param>
+        /// <returns><see cref="CustomerDetails"/>representation of a customer</returns>
+        Task<CustomerDetails> GetCustomerAsync(int customerId);
+
+        /// <summary>
+        /// Add Customer
+        /// </summary>
+        /// <param name="CustomerDetails">Customer to be created</param>
+        /// <returns><see cref="CustomerDetails"/>representation of added customer</returns>
+        Task<CustomerDetails> AddCustomerAsync(CustomerDetails customerDetails);
+    }
+}
---
+++ Database/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,22 @@
+﻿using DataTransfer;
+using System.Threading.Tasks;
+
+namespace Database.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        /// <summary>
+        /// Get a Customer
+        /// </summary>
+        /// <param name="customerId">ID of a Customer</param>
+        /// <returns><see cref="CustomerDetails"/>representation of a customer</returns>
+        Task<CustomerDetails> GetAsync(int customerId);
+
+        /// <summary>
+        /// Add a Customer
+        /// </summary>
+        /// <param name="customerId">Customer to be created</param>
+        /// <returns><see cref="CustomerDetails"/>representation of a customer</returns>
+        Task<CustomerDetails> AddAsync(CustomerDetails customerDetails);
+    }
+}
